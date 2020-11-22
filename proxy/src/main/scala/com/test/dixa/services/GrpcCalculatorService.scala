8f95b1d2952d1f7b7abfeeca53dff1d75123daa4
o.sharnikov <o.sharnikov@kryptonite.ru>
2020-11-22 21:46:21 +0300

    Move grpc client to Tageless final

--- proxy/src/main/scala/com/test/dixa/services/GrpcCalculatorService.scala
+++ proxy/src/main/scala/com/test/dixa/services/GrpcCalculatorService.scala
@@ -1,14 +1,14 @@
 package com.test.dixa.services
 
+import cats.syntax.functor._
 import cats.effect.{ConcurrentEffect, ContextShift, IO, Sync}
 import dixa.primes.{CalculatorFs2Grpc, CalculatorGrpc, Request}
 import fs2.{Chunk, Stream => FStream}
 import io.chrisdavenport.log4cats.Logger
-import io.grpc.ManagedChannelBuilder
+import io.grpc.{ManagedChannelBuilder, Metadata}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-//import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
 object GrpcCalculatorService {
   def build[F[_]: ConcurrentEffect: ContextShift: Logger]: F[GrpcCalculatorService[F]] =
@@ -25,25 +25,21 @@ class GrpcCalculatorService[F[_]: ConcurrentEffect: ContextShift: Logger] privat
 
     import org.lyranthe.fs2_grpc.java_runtime.implicits._
 
-//    val channel = ManagedChannelBuilder.forAddress("localhost", 9999).stream[F]
-//    val request = Request(10)
-//
-//    for {
-//      managedChannel <- channel
-//      client = CalculatorGrpc.stub(managedChannel)
-//      result = client.getPrimes(request)
-//      streamResult <- FStream.eval(result.)
-//    } yield streamResult
-    val channel = ManagedChannelBuilder.forAddress("localhost", 9999).usePlaintext().build
+    val channel = ManagedChannelBuilder
+      .forAddress("localhost", 9999)
+      .usePlaintext().stream[F]
     val request = Request(10)
 
-    val res = CalculatorGrpc.stub(channel).getPrimes(request)
+    val metaData = new Metadata
 
-    val pureRes = Await.result(res, Duration.Inf)
+    for {
+      managedChannel <- channel
+      client = CalculatorFs2Grpc.stub(managedChannel)
+      result = client.getPrimes(request, metaData).map(_.numbers)
+      streamResult <- FStream.evalSeq(result).covary[F]
+    } yield streamResult
 
-    FStream.emits(pureRes.numbers)
   }
-//    FStream.range(0, 5)
 
 
 
