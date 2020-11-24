f346c6e2754220dd5ff22bfd14659830f8301166
o.sharnikov <o.sharnikov@kryptonite.ru>
2020-11-24 09:24:59 +0300

    Clean Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,28 +6,26 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
   object Versions {
     val catsStm    = "0.8.0"
     val cats       = "2.2.0"
-    val catsEffect = "2.2.0"
     val tapir      = "0.17.0-M1"
     val http4s     = "0.21.9"
-    val circe      = "0.13.0"
     val logback    = "1.2.3"
     val log4cats   = "1.1.1"
     val fs2        = "2.4.4"
     val scalaTest  = "3.2.3"
-    val pureconfig = "0.14.0"
+    val pureConfig = "0.14.0"
   }
 
-  private def log4CatsLib(artifact: String): ModuleID  = "io.chrisdavenport"           %% artifact % Versions.log4cats
-  private def logbackLib(artifact: String): ModuleID   = "ch.qos.logback"               % artifact % Versions.logback
-  private def circeLib(artifact: String): ModuleID     = "io.circe"                    %% artifact % Versions.circe
-  private def http4sLib(artifact: String): ModuleID    = "org.http4s"                  %% artifact % Versions.http4s
-  private def tapirLib(artifact: String): ModuleID     = "com.softwaremill.sttp.tapir" %% artifact % Versions.tapir
-  private def fs2Lib(artifact: String): ModuleID       = "co.fs2"                      %% artifact % Versions.fs2
-  private def scalaTestLib(artifact: String): ModuleID = "org.scalatest"               %% artifact % Versions.scalaTest % Test
+  private def log4CatsLib(artifact: String): ModuleID   = "io.chrisdavenport"           %% artifact % Versions.log4cats
+  private def logbackLib(artifact: String): ModuleID    = "ch.qos.logback"               % artifact % Versions.logback
+  private def http4sLib(artifact: String): ModuleID     = "org.http4s"                  %% artifact % Versions.http4s
+  private def tapirLib(artifact: String): ModuleID      = "com.softwaremill.sttp.tapir" %% artifact % Versions.tapir
+  private def fs2Lib(artifact: String): ModuleID        = "co.fs2"                      %% artifact % Versions.fs2
+  private def pureConfigLib(artifact: String): ModuleID = "com.github.pureconfig"       %% artifact % Versions.pureConfig
+  private def scalaTestLib(artifact: String): ModuleID  = "org.scalatest"               %% artifact % Versions.scalaTest % Test
 
   private val cats = Seq(
     "org.typelevel"        %% "cats-core"   % Versions.cats,
-    "org.typelevel"        %% "cats-effect" % Versions.catsEffect,
+    "org.typelevel"        %% "cats-effect" % Versions.cats,
     "io.github.timwspence" %% "cats-stm"    % Versions.catsStm
   )
 
@@ -49,15 +47,7 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
     http4sLib("http4s-dsl"),
     http4sLib("http4s-blaze-server"),
     http4sLib("http4s-blaze-client"),
-    http4sLib("http4s-circe"),
     "com.softwaremill.sttp" %% "async-http-client-backend-fs2" % "1.6.7"
-  )
-
-  private val circe = Seq(
-    circeLib("circe-core"),
-    circeLib("circe-generic"),
-    circeLib("circe-parser"),
-    circeLib("circe-generic-extras")
   )
 
   private val log = Seq(
@@ -69,11 +59,14 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
     "net.logstash.logback" % "logstash-logback-encoder" % "4.11"
   )
 
+  private val pureConfig = Seq(
+    pureConfigLib("pureconfig"),
+    pureConfigLib("pureconfig-cats"),
+    pureConfigLib("pureconfig-cats-effect")
+  )
+
   private val other = Seq(
-    "io.grpc"                % "grpc-netty"             % "1.33.1",
-    "com.github.pureconfig" %% "pureconfig"             % Versions.pureconfig,
-    "com.github.pureconfig" %% "pureconfig-cats"        % Versions.pureconfig,
-    "com.github.pureconfig" %% "pureconfig-cats-effect" % Versions.pureconfig
+    "io.grpc" % "grpc-netty" % "1.33.1"
   )
 
   private val test = Seq(
@@ -83,9 +76,9 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
     "org.scalamock"  %% "scalamock"  % "4.4.0"  % Test
   )
 
-  val proxy = cats ++ tapir ++ http4s ++ other ++ circe ++ log ++ test ++ fs2
+  val proxy = cats ++ tapir ++ http4s ++ other ++ log ++ test ++ fs2 ++ pureConfig
 
-  val calculator = cats ++ tapir ++ http4s ++ other ++ circe ++ log ++ test ++ fs2
+  val calculator = cats ++ other ++ log ++ test ++ fs2 ++ pureConfig
 
-  val common = cats ++ tapir ++ http4s ++ other ++ circe ++ log ++ test ++ fs2
+  val common = cats ++ tapir ++ http4s ++ log ++ test
 }
