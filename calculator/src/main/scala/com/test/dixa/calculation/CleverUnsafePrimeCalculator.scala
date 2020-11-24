2ff263c94c00927907f545539e9932422e739452
o.sharnikov <o.sharnikov@kryptonite.ru>
2020-11-24 11:31:06 +0300

    Fix clever unsafe prime calculator

--- calculator/src/main/scala/com/test/dixa/calculation/CleverUnsafePrimeCalculator.scala
+++ calculator/src/main/scala/com/test/dixa/calculation/CleverUnsafePrimeCalculator.scala
@@ -13,18 +13,23 @@ object CleverUnsafePrimeCalculator {
 
 class CleverUnsafePrimeCalculator[F[_]: Applicative] private () extends PrimeCalculator[F] {
 
-  private var currentLast   = 1
-  private var currentPrimes = List(1)
+  private var currentLast   = 2
+  private var currentPrimes = List(2)
 
-  override def getPrimes(border: Int): F[List[Int]] = {
-    if (currentLast < border) {
-      currentPrimes = addMore(border: Int)
+  override def getPrimes(border: Int): F[List[Int]] =
+    if (border <= 1) List.empty[Int].pure[F]
+    else {
+      if (currentLast < border) {
+        currentPrimes ++= addMore(currentLast, border)
+        currentLast = border
+        currentPrimes.pure[F]
+      } else {
+        currentPrimes.takeWhile(_ <= border).pure[F]
+      }
+
     }
 
-    currentPrimes.takeWhile(_ <= border).pure[F]
-  }
-
-  def addMore(border: Int): List[Int] = {
+  def addMore(currentLast: Int, border: Int): List[Int] = {
     val primes = mutable.ArrayBuffer.fill(border + 1)(true)
 
     (2 to scala.math.sqrt(border).toInt).foreach { number =>
@@ -35,6 +40,9 @@ class CleverUnsafePrimeCalculator[F[_]: Applicative] private () extends PrimeCal
       }
     }
 
-    primes.zipWithIndex.drop(2).collect { case (isPrime, value) if isPrime => value }.toList
+    primes.zipWithIndex
+      .dropWhile { case (_, index) => index <= currentLast }
+      .collect { case (isPrime, value) if isPrime => value }
+      .toList
   }
 }
