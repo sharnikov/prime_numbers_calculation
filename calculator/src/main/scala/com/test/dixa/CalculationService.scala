0a38c0007351c183aca2f85764041c4ac4b3e88e
o.sharnikov <o.sharnikov@kryptonite.ru>
2020-11-24 23:13:09 +0300

    Add parameters to calculations config

--- calculator/src/main/scala/com/test/dixa/CalculationService.scala
+++ calculator/src/main/scala/com/test/dixa/CalculationService.scala
@@ -3,24 +3,25 @@ package com.test.dixa
 import cats.effect.{ Concurrent, Sync, Timer }
 import dixa.primes.{ CalculatorFs2Grpc, Request, Response }
 import com.test.dixa.calculation.PrimeCalculator
+import com.test.dixa.config.Config
 import fs2.{ Stream => FStream }
-
-import scala.concurrent.duration._
 
 object CalculationService {
   def build[F[_]: Concurrent: Timer, A](
-      primeCalculator: PrimeCalculator[F]
+      primeCalculator: PrimeCalculator[F],
+      config: Config
   ): F[CalculationService[F, A]] =
-    Sync[F].delay(new CalculationService[F, A](primeCalculator))
+    Sync[F].delay(new CalculationService[F, A](primeCalculator, config))
 }
 
 class CalculationService[F[_]: Concurrent: Timer, A] private (
-    primeCalculator: PrimeCalculator[F]
+    primeCalculator: PrimeCalculator[F],
+    config: Config
 ) extends CalculatorFs2Grpc[F, A] {
 
   override def getPrimes(request: Request, ctx: A): FStream[F, Response] =
     primeCalculator
       .getPrimes(request.number)
-      .groupWithin(100, 500.seconds)
+      .groupWithin(config.streamWindowParameters.amount, config.streamWindowParameters.time)
       .map(chunk => Response(chunk.toList))
 }
--- calculator/src/main/scala/com/test/dixa/config/Config.scala
+++ calculator/src/main/scala/com/test/dixa/config/Config.scala
@@ -5,14 +5,19 @@ import pureconfig.generic.semiauto.deriveReader
 import pureconfig.module.catseffect.syntax._
 import pureconfig.ConfigSource
 
+import scala.concurrent.duration.FiniteDuration
+
+case class StreamWindowParameters(amount: Int, time: FiniteDuration)
+
 case class ServerConfig(port: Int)
 
-case class Config(server: ServerConfig)
+case class Config(server: ServerConfig, streamWindowParameters: StreamWindowParameters)
 
 object Config {
 
-  implicit val serverConfigReader = deriveReader[ServerConfig]
-  implicit val configReader       = deriveReader[Config]
+  implicit val streamWindowParametersReader = deriveReader[StreamWindowParameters]
+  implicit val serverConfigReader           = deriveReader[ServerConfig]
+  implicit val configReader                 = deriveReader[Config]
 
   def parseConfig[F[_]: Sync: ContextShift](blocker: Blocker) =
     ConfigSource.default.loadF[F, Config](blocker)
